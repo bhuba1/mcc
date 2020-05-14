05744f3d75dc56656a2b36e728297d90699f7807
Balazsits Huba <balazsit@inf.u-szeged.hu>
2020-05-14 17:05:34 +0200

    menu + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoriesComponent } from './categories/categories.component';
+import { StatsComponent } from './stats/stats.component';
+import { AboutComponent } from './about/about.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'categories', component: CategoriesComponent},
+  {path: 'stats', component: StatsComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [CategoriesComponent, StatsComponent, AboutComponent]
